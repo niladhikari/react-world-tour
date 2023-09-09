ccce42b08382a48836715bbc20ccf730b9603104
niladhikari <niladhikari445@gmail.com>
2023-09-09 21:06:18 +0600

    fixed the error and add population and area display

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,14 @@
+/* eslint-disable react/prop-types */
 import './Country.css'
 const Country = ({country}) => {
     console.log(country);
-    const {name,flags} = country;
+    const {name,flags,population,area} = country;
     return (
         <div className='country'>
             <h4>Name : {name.common}</h4>
             <img src={flags.png} alt="" />
+            <p>Population : {population}</p>
+            <p>Area : {area}</p>
         </div>
     );
 };
